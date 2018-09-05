562e5503ae8c2bf24c4845ca8d9464f12cdea664
jamie <16000925@vcconnect.co.za>
2018-09-05 11:49:09 +0200

    setup sass and gulp

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # paranormal-minds
 source code for paranormal minds classroom system
+
+# requirements
+ 
+ node
+ 
+ 
+ # install dependencies
+ 
+ `npm install`
+ 
+ # run project
+  `gulp watch`
+
+ `dev_appserver.py .`
+ 
+ 
+ #deploy
+ `gulp styles`
+
+ `gcloud --project=paranormal-minds app deploy app.yaml
+`
---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat')
+
+gulp.task('styles', function(){
+    gulp.src('styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./static/css/'))
+})
+
+//Watch task
+gulp.task('watch',function() {
+    gulp.watch('styles/**/*.scss',['styles']);
+});
