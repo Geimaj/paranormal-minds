d4e5a899871c474a2c5f8c32d36bae2b9e47cf37
Your Name <you@example.com>
2018-09-07 13:20:15 +0200

    put in join course

--- src/router.py
+++ src/router.py
@@ -6,7 +6,8 @@ app = WSGIApplication(
     routes=[
         Route('/', handler='src.app.home.Home'),
         Route('/announcements', handler='src.app.announcements.Announcements'),
-        Route('/course/create', handler='src.app.createCourse.CreateCourse')
+        Route('/course/create', handler='src.app.createCourse.CreateCourse'),
+        Route('/course/join', handler='src.app.joinCourse.JoinCourse')
     ]
 )
 
