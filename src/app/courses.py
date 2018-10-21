a5bb09be16e13d20127e8191e0ce58dfb24a04dc
jamie <16000925@vcconnect.co.za>
2018-10-21 14:53:57 +0200

    update courses.html

--- src/app/courses.py
+++ src/app/courses.py
@@ -1,18 +1,18 @@
 from src.framework.request_handler import BaseRequestHandler
 import src.models as models
-from src.framework.api import decorator
+from src.framework.api import decorator, service
 
 class CoursesHandler(BaseRequestHandler):
 
     @decorator.oauth_required
     def get(self):
 
-        courses = models.Course.getCourses()
-
+        # courses = models.Course.getCourses()
+        courses = service.courses().list(courseStates=['ACTIVE', 'PROVISIONED']).execute(decorator.http())
 
         template_parms = {
             'title': 'My Courses',
-            'courses': courses
+            'courses': courses['courses']
         }
 
         self.render('course/courses.html', **template_parms)
