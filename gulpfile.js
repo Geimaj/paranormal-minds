48d64187e18b92ca94025b88f50a7e8c67771431
jamie <16000925@vcconnect.co.za>
2018-10-22 10:11:28 +0200

    add gulp for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat')
+var imagein = require('gulp-imagemin')
+var notify = require('gulp-notify')
 
 gulp.task('styles', function(){
     gulp.src('src/styles/**/*.scss')
@@ -14,4 +16,11 @@ gulp.task('styles', function(){
 //Watch task
 gulp.task('watch',function() {
     gulp.watch('src/styles/**/*.scss',['styles']);
+});
+
+gulp.task('images', function(){
+    return gulp.src('src/assets/**/*')
+        .pipe(imagein({ optimizationLevel: 3, progressive: true, interlaced: true })
+        .pipe(gulp.dest('./static/img/')))
+        .pipe(notify({ message: 'Images task complete' }));
 });
